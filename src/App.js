3349dfaa744805f3b4729bee5a58b3600bd33b01
hashibul1234 <hashibulhira2222@gmail.com>
2024-05-06 23:26:15 -0700

    first app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    number: 0,
+  };
+
+  constructor() {
+    super();
+
+    //this.increment = this.increment.bind(this);
+    //this.decrement = this.decrement.bind(this);
+  }
+
+  increment = () => {
+    // console.log("this.state.number");
+    // console.log("Increment function is called");
+
+    //const oldNumber = this.state.number;
+    //const newNumber = oldNumber + 1;
+
+    //const obj = { number: newNumber };
+
+    //this.setState(obj);
+    this.setState({ number: this.state.number + 1 });
+  };
+
+  decrement = () => {
+    //console.log("this.state.number");
+    //console.log("Decrement function is called");
+    //const oldNumber = this.state.number;
+    //const newNumber = oldNumber - 1;
+
+    //const newState = { number: newNumber };
+
+    //this.setState(newState);
+    if (this.state.number === 0) return;
+    this.setState({ number: this.state.number - 1 });
+  };
+
+  getDisableness = () => {
+    if (this.state.number === 0) return true;
+    return false;
+  };
+
+  render() {
+    return (
+      <>
+        <button className="btn btn-primary mt-6 my-1 mx-2"> ADD </button>
+
+        <div className="d-flex my-5" style={{ size: "10px" }}>
+          <button
+            onClick={this.decrement}
+            type="button"
+            className="btn btn-danger mx-2"
+            //disabled={this.getDisableness()}
+            //disabled={this.state.number === 0 ? true : false}
+            disabled={this.state.number === 0}
+          >
+            -
+          </button>
+          <p className="my-1">{this.state.number}</p>
+          <button
+            onClick={this.increment}
+            type="button"
+            className="btn btn-primary mx-2"
+          >
+            +
+          </button>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
